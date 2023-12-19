46751ebbeb7c61d2a4b510b26aea28632840eaf8
Bogdasaren <bogdanbogdanov305@gmail.com>
2023-12-19 22:20:32 +0800

    pre_final

--- defs.py
+++ defs.py
@@ -206,12 +206,120 @@ def find_altar():
             hp += r.randint(1, 3)
             printParameters()
 
-
-def initGame(initHp, initCoins, initDmg, initLvl):
+def barricade():
     global hp
-    global coins
-    global damage
-    global lvl
+
+    choice = input("""Проходя между горами, на вашем пути попадается обвал.
+    Обойти/Перелезть""").lower()
+    if choice == "обойти":
+        anoth_road = r.randint(1, 3)
+        if anoth_road == 1:
+            print("Вы нашли обходную дорогу, но что это там за звуки?..")
+            meetMonster()
+        else:
+            print("Вы нашли обходную дорогу.")
+    elif choice == "перелезть":
+        injury = r.randint(1, 3)
+        if injury == 1:
+            print("Перелезая через обвалившиеся камни, вы запинаетесь и больно падаете. Но обвал преодолён")
+            hp -= 1
+            printParameters()
+        else:
+            print("Вы успешно преодолели преграду")
+
+def victim():
+    global hp, damage, lvl
+
+    choice = input("""Вы находите алтарь для жертвоприношений и старинную книгу рядом.
+    В книге написано, что это место священно и боги одарят силой каждого кто осмелится пожертвовать собой ради них.
+        Рискнуть/Уйти""").lower()
+    if choice == "рискнуть":
+        chance = r.randint(1, 3)
+        if chance == 1:
+            print("""Проведя обряд, вы теряете сознание, но вскоре приходите в себя и чувствуете явный прилив сил.""")
+            hp += r.randint(3, 5)
+            damage += r.randint(3,5)
+            lvl_define()
+            printParameters()
+        else:
+            print("""Проведя обряд, вы теряете сознание... И больше не просыпаетесь.
+            Кажется боги вам не благоволят""")
+            hp = -1
+
+def storm():
+    global hp, coins
+    choice = input("""Идя по горам вы попадаете в сильную бурю. 
+    Вы можете рискнуть и пойти дальше или попытаться найти укрытие.
+    Идти/Прятаться""").lower()
+    if choice == "идти":
+        chance = r.randint(1, 3)
+        if chance == 1:
+            print("""Вы перебрались через гору и выбрались из стихии.
+            По пути вы нашли редкий цветок, который в ближайшей деревне продали алхимику, заработав немного денег.""")
+            coins += r.randint(3, 5)
+            printParameters()
+        else:
+            print("""Эх... Дурак и ветер... Битва со стихией прошла не в твою пользу.""")
+            hp -= -1
+    elif choice == "прятаться":
+        print("Ты успешно переждал бурю, можно идти дальше")
+
+def wolf_trouble():
+    global hp, coins, damage, lvl
+    choice = input("""Вы слышите впереди крики и волчий вой. 
+    Вы решаете:
+    Помочь/Уйти""").lower()
+    if choice == "помочь":
+        print("""Вы бежите на звуки и видите парня и девушку. Перед ними разъярённый волк и труп растерзанный этим волком.
+        Вы выхватываете меч и бросаетесь на волка!""")
+        wolfLvl = r.randint(1, 4)
+        wolfHp = wolfLvl * r.randint(1, 2)
+        wolfDmg =wolfLvl * 2 - 1
+        print("""У волка {0} уровень, у него {1} здоровья и {2} урона.""".format( wolfLvl, wolfHp, wolfDmg))
+        printParameters()
+
+        while wolfHp > 0:
+            choice = input("""Что решаешь?
+                АТАКА/Тикаем -->""").lower()
+            if choice == "атака":
+                wolfHp -= damage
+                print("""Отличный удар!
+                    У твари осталось""", wolfHp, "жизней.")
+            elif choice == "тикаем":
+                chance = r.randint(0, 1)
+                if chance == 0:
+                    print("Ты дал этим людям надежду и бросил их! Ты хуже люого чудовища в этом жестоком мире")
+                    break
+                else:
+                    print("Не стоило бросать этих людей. Ты стал закуской")
+                    hp = -1
+                    break
+            else:
+                continue
+
+            if wolfHp > 0:
+                hp -= wolfDmg
+                print("А он не промах! Атаковал тебя! Это было больно, у тебя", hp, "здоровья")
+            if hp <= 0:
+                break
+        else:
+            reward = input("""Девушка смотрит на тебя со слезами на глазах и не может произнести ни слова.
+        Мужчина подходит к тебе, благодарит и протягивает кошель с золотом.
+        Взять/Отказаться""").lower()
+            if reward == "взять":
+                coins += r.randint(3, 5)
+                printParameters()
+            elif reward == "отказаться":
+                print("""Мужчина хвалит тебя за твой выбор, но говорит, что не может оставить тебя без награды.
+                Он достаёт из-за пазухи амулет и даёт его тебе.
+                Ты получаешь 3 здоровья""")
+                hp += 3
+            lvl_define()
+            printParameters()
+    else:
+        print("Своя шкура дороже.")
+def initGame(initHp, initCoins, initDmg, initLvl):
+    global hp, coins, damage, lvl
 
     hp = initHp
     coins = initCoins
@@ -228,7 +336,7 @@ def lvl_define():
 
 
 def gameLoop():
-    situation = r.randint(0, 10)
+    situation = r.randint(0, 15)
     if situation == 0:
         meetShop()
     elif situation == 1:
@@ -239,16 +347,15 @@ def gameLoop():
         find_item()
     elif situation == 4:
         find_altar()
+    elif situation == 5:
+        barricade()
+    elif situation == 6:
+        victim()
+    elif situation == 7:
+        storm()
+    elif situation == 8:
+        wolf_trouble()
     else:
         input("Блуждаю...")
 
 initGame(6, 5, 2, 1)
-
-# while True:
-#     gameLoop()
-#
-#     if hp <= 0:
-#         if input("Хочешь начать сначала? (Да/Нет):").lower() == "да":
-#             initGame(6, 5, 2, 1)
-#         else:
-#             break
